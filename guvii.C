578638e7f5a610c233cadab4a0694e8270c46f92
manjupriyaR <32294836+manjupriyaR@users.noreply.github.com>
2017-09-27 11:52:50 +0530

    Create guvii.C

---
+++ guvii.C
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conoi.h>
+int main()
+{
+char d;
+printf("enter the character");
+scanf("%c",&d);
+if(c>='a'&&c<'z'||c>='A'&&c<='Z')
+printf("%c is an alphabet",d);
+else
+printf("%c is not an alphabet",d);
+return 0;
+}
